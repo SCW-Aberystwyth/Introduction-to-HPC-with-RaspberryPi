1ed73025a8b43d1bf9f1112929c8fee6e739c822
Colin Sauze <cos@aber.ac.uk>
2018-06-13 12:18:53 +0100

    adding graphs of performance times

--- fig/pymp_timings.py
+++ fig/pymp_timings.py
@@ -6,7 +6,7 @@ import pandas
 print("imports done")
 
 
-df = pandas.read_csv('mpi_timings.csv',index_col=0)
+df = pandas.read_csv('pymp_timings.csv',index_col=0)
 
 df.plot(legend=None)
 
@@ -14,13 +14,13 @@ print("plot made")
 
 plt.xlabel('Cores')
 ax = df.plot(legend=None)
-ax.set_ylim(0,26)
-ax.set_xticks(range(2,49,4))
-ax.set_yticks(range(0,30,2))
+ax.set_ylim(0,300)
+ax.set_xticks(range(1,17,1))
+ax.set_yticks(range(0,300,30))
 ax.set_ylabel('Time')
-ax.set_title('Time vs Cores MPI')
+ax.set_title('Time vs Cores PyMP')
 ax.grid()
 
-plt.savefig('mpi.png',dpi=200)
+plt.savefig('pymp.png',dpi=200)
 
 print("saved")
